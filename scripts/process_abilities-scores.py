efad82cf95adfe4808a51424cd8311f86d618c5d
ashwinrai-il <ashwin.rai@ivadolabs.com>
2025-03-13 11:07:15 -0400

    chore: added trasform recipe to makefile

--- scripts/process_abilities-scores.py
+++ scripts/process_abilities-scores.py
@@ -28,18 +28,25 @@ if __name__ == "__main__":
 
     # Normalize the JSON data to flatten it
     df = pd.json_normalize(data, sep="_")
+
+    # Check if the JSON data has the expected schema
     assert set(df.columns.to_list()) == set(
         ["index", "name", "full_name", "desc", "skills", "url"]
     ), "Unexpected data organization, schema has probably changed"
+
+    # Concatenate the description columns and drop the index column
     df = df.assign(
         desc=lambda temp: temp[description_col_str].apply(lambda x: "".join(x))
     ).drop(original_index_col_str, axis=1)
 
+    # Explode the skills column since each record is a dictionary
     exploded_skills = (
         df[[skills_col_str, abilities_col_str]]
         .explode(skills_col_str)
         .rename(columns={abilities_col_str: "ab_name"})
     )
+
+    # Group all the skill descriptions by ability and prep the dataset for joining with the ability table
     skills = (
         pd.concat(
             [
@@ -54,6 +61,8 @@ if __name__ == "__main__":
         .sum()
         .rename(columns={skills_key_name: "skill_names"})
     )
+
+    # Join the ability table with the newly grouped abilities table
     output_df = (
         pd.merge(df, skills, how="left", left_on=abilities_col_str, right_on="ab_name")
         .drop([skills_col_str, urls_col_str], axis=1)
@@ -61,3 +70,7 @@ if __name__ == "__main__":
         .fillna("none")
         .rename(columns={abilities_col_str: "ability_name"})
     )
+
+    # Save the table as a parquet file
+    save_folder.mkdir(parents=True, exist_ok=True)
+    output_df.to_parquet(save_folder / f"{ruleset}.parquet")
